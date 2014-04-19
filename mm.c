6f88347e0d1115c495f3ce408e5519364624c2d2
tonocm <hcardena@u.rochester.edu>
2014-04-18 22:50:56 -0400

    i dont like you jake

--- mm.c
+++ mm.c
@@ -44,11 +44,60 @@ team_t team = {
 
 #define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
 
+
+/* Global Variables*/
+static char * mem_heap; /* Ptr to first byte of the heap */
+static char * mem_brk; /* Ptr to last byte of the heap + 1 */
+static char * mem_max_addr; /* Max legal heap addr + 1 */
+
+
+/*Figure 9.43 from textbook (pg. 830) tips*/
+
+/*Basic constants and macros*/
+#define WSIZE 4 /* Word and header/footer size (bytes) */
+#define DSIZE 8 /* Double word size (bytes) */
+#define CHUNKSIZE (1 << 12) /* Extend heap by this amount (bytes) */
+#define MAX(x,y) ((x) > (y)? (x) : (y))
+
+/* Pack a size and allocated bit into a word */
+#define PACK(size, alloc) ((size) | (alloc))
+
+/* Read and write a word at address p */
+#define GET(p) (*(unsigned int *)(p))
+#define PUT(p, val) (*(unsigned int *)(p) = (val))
+
+
+/* Read the size and allocated fields from address p */
+#define GET_SIZE(p) (GET(p) & ~0x7)
+#define GET_ALLOC(p) (GET(p) & 0x1)
+
+/* Given block ptr bp, computer address of its header and footer */
+#define HDRP(bp) ((char *)(bp) - WSIZE)
+#define FTRP(bp) ((char *)(bp) + GET_SIZE(HDRP(bp)) - DSIZE)
+
+/*Given block ptr bp, compute address of next and previous blocks */
+#define NEXT_BLCKP(bp) ((char *)(bp) + GET_SIZE(((char *)(bp) - WSIZE)))
+#define PREV_BLKP(bp) ((char *)(bp) - GET_SIZE(((char *)(bp) - DSIZE)))
+
+/*Max Heap Size */
+#define MAX_HEAP (20*(1<<20)) /*20 MB*/
+
 /* 
  * mm_init - initialize the malloc package.
  */
 int mm_init(void)
 {
+  if((heap_listp = mem_sbrk(4*WSIZE)) == (void *)-1)
+    return -1;
+  PUT(heap_listp, 0); /* Alignment padding */
+  PUT(heap_listp + (1*WSIZE), PACK(DSIZE, 1)); /* Prologue header */
+  PUT(heap_listp + (2*WSIZE), PACK(DSIZE, 1)); /* Prologue footer */
+  PUT(heap_listp + (3*WSIZE), PACK(0    , 1)); /* Epilogue header */
+  heap_listp += (2*WSIZE);
+
+  /* Extend the empty heap with a free block of CHUNKSIZE bytes */
+  if(extended_heap(CHUNKSIZE/WSIZE) == NULL)
+    return -1;
   return 0;
 }
 
